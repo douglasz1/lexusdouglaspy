ed442bdc587be74e2831959a675e36886e5fa939
douglasz1 <douglas.santana.dev@gmail.com>
2020-12-18 16:27:51 -0300

    make it better

---
+++ send_mail.py
@@ -0,0 +1,24 @@
+
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_mail(customer, dealer, rating, comments):
+    port = 2525
+    smtp_server = 'smtp.mailtrap.io'
+    login = 'db40ab0c23e117'
+    password = 'd3325c1130cd90'
+    message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
+
+    sender_email = 'douglas.santana.dev@gmail.com'
+    receiver_email = 'douglas.santana.dev@gmail.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Lexus Feedback'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+
+    # Send email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
